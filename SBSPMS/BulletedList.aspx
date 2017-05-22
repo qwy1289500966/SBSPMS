829d34e4da446275e02d945a8d57dc9b52f4dc2e
qwy1289500966 <qwy307@126.com>
2017-05-22 11:50:43 +0800

    例4-9
    BulletedList

---
+++ SBSPMS/BulletedList.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="BulletedList.aspx.cs" Inherits="SBSPMS.BulletedList" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:BulletedList ID="BulletedList1" runat="server" BulletStyle="Numbered" 
+            DisplayMode="LinkButton" onclick="BulletedList1_Click">
+            <asp:ListItem Text="中国达人秀" />
+            <asp:ListItem Text="超级女声" />
+            <asp:ListItem Text="我型我秀" />
+            <asp:ListItem Text="舞林争霸" />
+        </asp:BulletedList>
+    <asp:Label ID="Label1" runat="server" Text="Label" Visible="false"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ SBSPMS/BulletedList.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SBSPMS
+{
+    public partial class BulletedList : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BulletedList1_Click(object sender, BulletedListEventArgs e)
+        {
+            Label1.Visible = true;
+            switch (e.Index)
+            {
+                case 0: Label1.Text = "你最喜欢的选秀节目是中国达人秀！";
+                    break;
+                case 1: Label1.Text = "你最喜欢的选秀节目是超级女声！";
+                    break;
+                case 2: Label1.Text = "你最喜欢的选秀节目是我型我秀！";
+                    break;
+                case 3: Label1.Text = "你最喜欢的选秀节目是舞林争霸！";
+                    break;
+                default: Label1.Text = "你的选择错误！";
+                    break;
+            }
+        }
+    }
+}
