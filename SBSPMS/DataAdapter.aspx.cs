096ab271c3f6f27dc1427c28d3b8ccb82e04dd51
qwy1289500966 <qwy307@126.com>
2017-05-23 12:03:40 +0800

    例6-3
    SqlDataAdapter

---
+++ SBSPMS/DataAdapter.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace SBSPMS
+{
+    public partial class DataAdapter : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string str = "Data Source=192.168.50.4;Initial Catalog=SBSPMS;Persist Security Info=True;User ID=sa;Password=123456";
+            SqlConnection myConnection = new SqlConnection(str);
+            myConnection.Open();
+            SqlCommand myCommand = new SqlCommand("select * from BookInfo",myConnection);
+            SqlDataAdapter Adapter = new SqlDataAdapter();
+            Adapter.SelectCommand = myCommand;
+            DataSet myDs = new DataSet();
+            Adapter.Fill(myDs);
+            DataTable myTable = myDs.Tables[0];
+            Response.Write("<h3>使用DataSet和DataAdapter查询数据库</h3>");
+            Response.Write("<table border=1 cellspacing=0 cellpadding=2>");
+            Response.Write("<tr bgcolor=yellow>");
+            foreach (DataColumn myColumn in myTable.Columns)
+            {
+                Response.Write("<td>"+myColumn.ColumnName+"</td>");
+            }
+            Response.Write("</tr>");
+            foreach (DataRow myRow in myTable.Rows)
+            {
+                Response.Write("<tr>");
+                foreach (DataColumn myColumn in myTable.Columns)
+                {
+                    Response.Write("<td>"+myRow[myColumn]+"</td>");
+                }
+                Response.Write("</tr>");
+            }
+            Response.Write("</table>");
+            myConnection.Close();
+        }
+    }
+}
