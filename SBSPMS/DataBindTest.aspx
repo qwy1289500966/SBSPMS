a40e0655df8bc04b00eab99d9268c6fcfb4e5e02
qwy1289500966 <qwy307@126.com>
2017-05-23 15:54:41 +0800

    例7-8
    DataBind

---
+++ SBSPMS/DataBindTest.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DataBindTest.aspx.cs" Inherits="SBSPMS.DataBindTest" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <h3>
+        变量的绑定
+    </h3>
+    你好：<asp:Label ID="Label1" Text="<%#Name %>" runat="server" /><br />
+    登录时间：<asp:Label ID="Label2" Text="<%#LoginTime %>" runat="server" /><br />
+    <h3>
+        绑定表达式</h3>
+    求整数1-100的总和及平均数<br />
+    总和=<asp:Label Text="<%#sum %>" runat="server" />
+    平均数=<asp:Label Text="<%#sum/100 %>" runat="server" /><br />
+    <h3>
+        绑定集合
+    </h3>
+    <asp:DataGrid ID="DataGrid1" runat="server" DataSource="<%#myArray %>">
+    </asp:DataGrid><br />
+    <h3>
+        绑定方法
+    </h3>
+    (1-100整数和)*2=<asp:Label Text="<%#getSum() %>" runat="server" /><br />
+    <h3>
+        下拉控件的数据绑定<br />
+    </h3>
+    <asp:DropDownList runat="server" AutoPostBack="true" ID="DropDownList1"
+        onselectedindexchanged="Unnamed5_SelectedIndexChanged">
+    </asp:DropDownList><br />
+    <asp:Label ID="Label3" runat="server" /><br />
+    <h3>
+        绑定到ListBox
+    </h3>
+    <asp:ListBox runat="server" ID="ListBox1" Height="88px">
+    </asp:ListBox>
+    </form>
+</body>
+</html>
---
+++ SBSPMS/DataBindTest.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Collections;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace SBSPMS
+{
+    public partial class DataBindTest : System.Web.UI.Page
+    {
+        public string Name = "管理员";
+        public DateTime LoginTime = DateTime.Now;
+        protected int sum = 0;
+        protected ArrayList myArray = new ArrayList();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                sum = getSum();
+                myArray.Add("春天：Spring");
+                myArray.Add("夏天：Summer");
+                myArray.Add("秋天：Autumn");
+                myArray.Add("冬天：Winter");
+                DropDownList1.DataSource = myArray;
+                DropDownList1.DataBind();
+                DataGrid1.DataBind();
+
+                string constr = "Data Source=192.168.50.4;Initial Catalog=SBSPMS;Persist Security Info=True;User ID=sa;Password=123456";
+                string str = "select * from BookInfo";
+                SqlConnection con = new SqlConnection(constr);
+                con.Open();
+                SqlDataAdapter sda = new SqlDataAdapter(str,con);
+                DataSet ds = new DataSet();
+                sda.Fill(ds,"BookInfo");
+                ListBox1.DataSource=ds.Tables["BookInfo"];
+                ListBox1.DataTextField = "Name";
+                ListBox1.DataValueField = "ID";
+                ListBox1.DataBind();
+                con.Close();
+            }
+            //Page.DataBind();
+        }
+        protected int getSum()
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+
+        protected void Unnamed5_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Label3.Text = "现在的季节是："+DropDownList1.SelectedValue;
+        }
+    }
+}
